ab81772b9da960d8bbde7fe4d6ca7a626ddaa7d0
anirban-s <baban.chrome@gmail.com>
2019-08-12 11:04:32 +0530

    firebase users db is created and pulling data from db to code

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
     appId: "1:878466633272:web:d6d556c4908eab3c"
   };
 
+export const createUserProfileDocument = async (userAuth, aditionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...aditionalData
+      })
+    } catch (error) {
+      console.log('error creating user ' , error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
