dc1178cb80cc7c532405f9634b9a79a53b371acf
anirban-s <baban.chrome@gmail.com>
2019-08-25 04:01:11 +0530

    creadted nested routing in shop page

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,12 +1,10 @@
 import SHOP_DATA from './shop.data';
 
 const INITIAL_STATE = {
-    
     collections: SHOP_DATA
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
-    console.log('tets',SHOP_DATA)
     switch(action.type){
         default:
             return state;
